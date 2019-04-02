70aaa394de0541847f88237a8035816f60ebe1d6
Diana Marcela <dm.gutierrez11@uniandes.edu.co>
2019-04-02 13:40:37 -0500

    render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {IntlProvider, addLocaleData} from 'react-intl';
+import { IntlProvider, addLocaleData } from "react-intl";
 
 import JobsList from "./components/jobsList";
-import esLocaleData from 'react-intl/locale-data/es';
+import esLocaleData from "react-intl/locale-data/es";
 import localeEsMessages from "./locales/es";
 
 addLocaleData(esLocaleData);
 
 ReactDOM.render(
-	<IntlProvider locale="es-ES" messages= {localeEsMessages}>
+  <div>
+    <IntlProvider locale="es-ES" messages={localeEsMessages}>
+      <JobsList />
+    </IntlProvider>
+    <IntlProvider locale="en">
 		<JobsList/>
-	</IntlProvider>, document.getElementById("root")
+	</IntlProvider>
+  </div>,
+  document.getElementById("root")
 );
