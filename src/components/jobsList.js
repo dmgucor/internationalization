a9cf3f8f58e5f88012a2c171fcf04e31853b3ada
Diana Marcela <dm.gutierrez11@uniandes.edu.co>
2019-04-02 13:29:19 -0500

    añadir español

--- src/components/jobsList.js
+++ src/components/jobsList.js
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 import Job from "./job";
+import { FormattedMessage } from "react-intl";
 
 export default class JobsList extends React.Component {
-
-  state = { 
-  	"offers": [
-    	{
-    	  "id": "0001",
-        "name": "Manager",
-    	  "company": "Schneider Electric", 
-    	  "salary": 4.5,
-    	  "city": "Bogotá, Colombia",
-        "date": "2019-03-26",
-        "views": 1672
-      }, 
+  state = {
+    offers: [
       {
-        "id": "0002",
-        "name": "Software Engineer",
-        "company": "Google Inc.", 
-        "salary": 20,
-        "city": "Palo Alto, CA, USA",
-        "date": "2019-03-27",
-        "views": 576
+        id: "0001",
+        name: "Manager",
+        company: "Schneider Electric",
+        salary: 4.5,
+        city: "Bogotá, Colombia",
+        date: "2019-03-26",
+        views: 1672
       },
       {
-        "id": "0003",
-        "name": "Nurse",
-        "company": "Clínica La Aurora", 
-        "salary": 1,
-        "city": "Cali, Colombia",
-        "date": "2019-03-28",
-        "views": 4332
+        id: "0002",
+        name: "Software Engineer",
+        company: "Google Inc.",
+        salary: 20,
+        city: "Palo Alto, CA, USA",
+        date: "2019-03-27",
+        views: 576
+      },
+      {
+        id: "0003",
+        name: "Nurse",
+        company: "Clínica La Aurora",
+        salary: 1,
+        city: "Cali, Colombia",
+        date: "2019-03-28",
+        views: 4332
       }
     ]
   };
@@ -42,16 +42,30 @@ export default class JobsList extends React.Component {
           <thead className="thead-dark">
             <tr>
               <th scope="col">#</th>
-              <th scope="col">Position</th>
-              <th scope="col">Company</th>
-              <th scope="col">Salary</th>
-              <th scope="col">City</th>
-              <th scope="col">Publication date</th>
-              <th scope="col">Views</th>
+              <th scope="col">
+                <FormattedMessage id="Position" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Company" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Salary" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="City" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="PublicationDate" />
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Views" />
+              </th>
             </tr>
           </thead>
           <tbody>
-              {this.state.offers.map( (e,i) => <Job key={i} offer={e}/>)}
+            {this.state.offers.map((e, i) => (
+              <Job key={i} offer={e} />
+            ))}
           </tbody>
         </table>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {IntlProvider} from 'react-intl';
+import {IntlProvider, addLocaleData} from 'react-intl';
 
 import JobsList from "./components/jobsList";
+import esLocaleData from 'react-intl/locale-data/es';
+import localeEsMessages from "./locales/es";
+
+addLocaleData(esLocaleData);
 
 ReactDOM.render(
-	<IntlProvider locale="en">
+	<IntlProvider locale="es-ES" messages= {localeEsMessages}>
 		<JobsList/>
 	</IntlProvider>, document.getElementById("root")
 );
